0399a327b1cb22017a830553860ebe320db11890
schnae1 <schnaeric@gmail.com>
2019-09-27 12:21:01 -0700

    added loading wheel and updated displaying results

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import axios from 'axios';
+import ReactLoading from 'react-loading';
+import '../styles/Home.css';
 
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            loading: false,
             total: 0,
             location: '',
             distance: '',
-            result: '',
-            business: '',
+            result: ''
         };
         this.handleLocation = this.handleLocation.bind(this);
         this.handleDistance = this.handleDistance.bind(this);
@@ -37,14 +39,13 @@ class HomePage extends React.Component {
               categories: 'restaurants',
          }});
 
-        this.setState({ result: resp, business: resp.data.businesses[0]['name'] });
-
-        console.log(resp.data.total);
-        for(let i = 0; i < resp.data.businesses.length; i++){
-            console.log(resp.data.businesses[i]['name']);
-        }
-
-        this.setState({ total: 0, location: '', distance: '' });
+        this.setState({ 
+            result: resp, 
+            loading: false, 
+            location: '', 
+            distance: '',
+            total: resp.data.total
+        });
          
     }
 
@@ -61,20 +62,57 @@ class HomePage extends React.Component {
     }
 
     handleClick() {
+        this.setState({ loading: true });
         this.getRestaurants();
     }
 
     render() {
+        let results;
+
+        if(this.state.loading){
+            results = <Loading />;
+        } else {
+            results = <Restaurants rest={this.state.result} />;
+        }
         return (
-            <div id="h-container">
-                <h1>Home</h1>
-                <input type="text" placeholder="Your Current Location" value={this.state.location} onChange={this.handleLocation} />
-                <input type="text" placeholder="Search Radius in Miles" value={this.state.distance} onChange={this.handleDistance} />
-                <button onClick={this.handleClick}>Find</button>
-                <p>{this.state.business}</p>
+            <div id="home-container">
+                <div id="header">
+                    <h1>Foodies</h1>
+                    <input type="text" placeholder="Your Current Location" value={this.state.location} onChange={this.handleLocation} />
+                    <input type="text" placeholder="Search Radius in Miles" value={this.state.distance} onChange={this.handleDistance} />
+                    <button onClick={this.handleClick}>Find</button>
+                </div>
+                <div id="display-rest">
+                    {results}
+                </div>
             </div>
         );
     }
+}
+
+const Restaurants = (props) => {
+    if(props.rest !== ''){
+        const restaurants = props.rest.data.businesses;
+        const displayItems = restaurants.map((rest) =>
+            <div key={rest.id} className="rest-card">
+                <p>{rest['name']}</p>
+            </div>
+        );
+        return displayItems;
+    } else {
+        return (
+            <div className="rest-card">
+                <h2>No Results</h2>
+            </div>
+        );
+    }
+    
+}
+
+const Loading = () => {
+    return (
+        <ReactLoading type="spinningBubbles" color="blue" />
+    );
 }
 
 export default HomePage;
